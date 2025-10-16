0d4a9b22d36218fe9988411497aa57cd38430969
John B <49559203+johnB007@users.noreply.github.com>
2025-10-16 15:06:13 -0400

    Update All_Things_MDAV.cmd

--- All_Things_MDAV.cmd
+++ All_Things_MDAV.cmd
@@ -1,1 +1,3 @@
+## MDAV security intelligence and product updates
+Update your antivirus protection, even if Microsoft Defender Antivirus is running in passive mode. You can find the lates engine, platform, and signature date in Security intelligence updates for Microsoft Defender Antivirus and other Microsoft anti-malware
 
